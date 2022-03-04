ca7428ad0d594c986507613b42645affe08a43a6
FoxerBR <andrebarbosadg@gmail.com>
2022-03-04 13:49:50 -0300

    Seller entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,13 +1,17 @@
 package application;
 
+import java.util.Date;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
-		Department dep = new Department(01, "Tecnology");
+		Department dep = new Department(01, "Books");
+		Seller sel = new Seller(21, "Bob", "bob@gmail.com", new Date(), 3000.0, dep);
 		
-		System.out.println(dep);
+		System.out.println(sel);
 
 	}
 
